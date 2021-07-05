c7e09413f351365cb093d5e1a8489244566ab24e
Felipe <fgrgod1@gmail.com>
2021-07-05 10:41:42 -0300

    Estudos For e While

---
+++ for.js
@@ -0,0 +1,7 @@
+const trocarPneu = (pneu) => {
+    console.log('Trocando pneu ' + pneu)
+}
+
+for (let pneuAtual = 1; pneuAtual <= 4; pneuAtual++) {
+    trocarPneu(pneuAtual);
+}
---
+++ while.js
@@ -0,0 +1,6 @@
+let numero = 0;
+
+while (numero < 20) {
+    console.log(numero);
+    numero++;
+}
